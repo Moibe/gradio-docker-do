03175f3d24c38552a5a37549a6d0bee35c84c97b
Moibe <moi_estrello@hotmail.com>
2024-10-01 23:14:11 -0600

    Bonjour

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
 def greet(name):
-    return f"Hello, Hola, Tervetuloa {name}!"
+    return f"Hello, Hola, Tervetuloa, Bonjour {name}!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch()
