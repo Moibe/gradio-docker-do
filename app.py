cbf0dbbfa23c0c35fbecf132a770d2347a34a24b
Moibe <moi_estrello@hotmail.com>
2024-10-05 15:05:42 -0600

    Test Bash Running Ok!

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
 def greet(name):
-    return f"Hello, Hola, Tervetuloa, Bonjour, Danke, Spasiva {name} !"
+    return f"Hello, Hola, Tervetuloa, Bonjour, Danke, {name} !"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch()
