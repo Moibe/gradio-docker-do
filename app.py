a369a9cedc6d768dad646c71ccb96e638f17ae21
Moibe <moi_estrello@hotmail.com>
2024-10-05 14:04:58 -0600

    Test distracted_wilbur

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
 def greet(name):
-    return f"182Hello, Hola, Tervetuloa, Bonjour, Danke, Spasiva {name} !"
+    return f"Hello, Hola, Tervetuloa, Bonjour, Danke, Spasiva {name} !"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text").launch()
